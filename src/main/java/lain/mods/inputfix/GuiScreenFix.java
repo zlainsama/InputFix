8ab1cf625c84e3a7b11774323f649d083523c147
玲音 <zlainsama@163.com>
2014-10-09 17:58:16 +0800

    cleanup

--- src/main/java/lain/mods/inputfix/GuiScreenFix.java
+++ src/main/java/lain/mods/inputfix/GuiScreenFix.java
@@ -1,24 +1,22 @@
 package lain.mods.inputfix;
 
-import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import lain.mods.inputfix.interfaces.IGuiScreen;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
 import org.lwjgl.input.Keyboard;
 
 public class GuiScreenFix
 {
 
-    private static Field mc;
     private static Method keyTyped;
+
+    private static IGuiScreen proxy;
+    private static GuiScreen currentGui;
 
     static
     {
         try
         {
-            mc = GuiScreen.class.getDeclaredField(InputFix.RUNTIME_DEOBF ? "field_146297_k" : "mc");
-            mc.setAccessible(true);
             keyTyped = GuiScreen.class.getDeclaredMethod(InputFix.RUNTIME_DEOBF ? "func_73869_a" : "keyTyped", char.class, int.class);
             keyTyped.setAccessible(true);
         }
@@ -26,41 +24,35 @@ public class GuiScreenFix
         {
             throw new RuntimeException(t);
         }
+
+        proxy = new IGuiScreen()
+        {
+            @Override
+            public void keyTyped(char c, int k)
+            {
+                try
+                {
+                    if (currentGui != null)
+                        keyTyped.invoke(currentGui, c, k);
+                }
+                catch (Throwable t)
+                {
+                    throw new RuntimeException(t);
+                }
+            }
+        };
     }
 
     public static void handleKeyboardInput(GuiScreen gui)
     {
-        try
-        {
-            if (InputFixSetup.impl != null)
-            {
-                final Object obj = gui;
-                InputFixSetup.impl.handleKeyboardInput(new IGuiScreen()
-                {
-                    public void keyTyped(char c, int k)
-                    {
-                        try
-                        {
-                            keyTyped.invoke(obj, c, k);
-                        }
-                        catch (Throwable t)
-                        {
-                            throw new RuntimeException(t);
-                        }
-                    }
-                });
-            }
-            else if (Keyboard.getEventKeyState())
-            {
-                keyTyped.invoke(gui, Keyboard.getEventCharacter(), Keyboard.getEventKey());
-            }
+        currentGui = gui;
 
-            ((Minecraft) mc.get(gui)).dispatchKeypresses();
-        }
-        catch (Throwable t)
-        {
-            throw new RuntimeException(t);
-        }
+        if (InputFixSetup.impl != null)
+            InputFixSetup.impl.handleKeyboardInput(proxy);
+        else if (Keyboard.getEventKeyState())
+            proxy.keyTyped(Keyboard.getEventCharacter(), Keyboard.getEventKey());
+
+        gui.mc.dispatchKeypresses();
     }
 
 }
